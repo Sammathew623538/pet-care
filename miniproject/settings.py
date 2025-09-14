6d2fdada274c400ecd7d354c0c5a858be7ec3d68
Sam Mathew <sammathew@example.com>
2025-09-14 09:01:30 +0530

    Initial commit - PetCare project

--- miniproject/settings.py
+++ miniproject/settings.py
@@ -103,8 +103,8 @@ AUTH_PASSWORD_VALIDATORS = [
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'sammathew8108@gmail.com'
-EMAIL_HOST_PASSWORD = 'mgqfgxstcklxgmrs'
+EMAIL_HOST_USER = 'enter your email'
+EMAIL_HOST_PASSWORD = 'enter password'
 EMAIL_USE_TLS = True
 
 
